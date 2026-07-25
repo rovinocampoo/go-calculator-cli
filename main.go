9feb8e2f2af3764fe3a371f676fef6706096187b
Kurovin <rovinocampoo@gmail.com>
2026-07-26 00:25:53 +0800

    Added simple error trap on division and changed outputs to float

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, Welcome to Rovin's first Go project. The Mighty Calculator!")
 
-	var firstNum float64 
+	var firstNum float64
 	var secondNum float64
 
 	fmt.Print("Enter First Number:")
@@ -13,16 +13,18 @@ func main() {
 	fmt.Print("Enter Second Number:")
 	fmt.Scan(&secondNum)
 
-	sum := firstNum+secondNum
-	diff := firstNum-secondNum
+	sum := firstNum + secondNum
+	diff := firstNum - secondNum
 	mult := firstNum * secondNum
-	div := firstNum/secondNum
+	div := firstNum / secondNum
 
-	fmt.Println("Sum:" ,sum)
-	fmt.Println("Difference:" ,diff)
-	fmt.Println("Product:" ,mult)
-	fmt.Println("Quotient:" ,div)
-
-
+	fmt.Printf("Sum: %.2f\n", sum)
+	fmt.Printf("Difference: %.2f\n", diff)
+	fmt.Printf("Product: %.2f\n", mult)
+	if secondNum != 0 {
+		fmt.Printf("Quotient: %.2f\n", div)
+	} else {
+		fmt.Println("Can't divide by zero good sir.")
+	}
 
 }
